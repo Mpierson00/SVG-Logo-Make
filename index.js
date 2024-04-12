1a2a891098f9c56839af111095e0f2ddade039f3
Michael <mpierson00@gmail.com>
2024-04-12 12:04:51 -0400

    added text questions

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const { Circle, Square, Triangle } = require('./lib/shapes');
 
 const questions = [
     {
+      type: 'input',
+      name: 'text',
+      message: 'What text do you want on the logo (up to 3 characters)?',
+      validate: input => input.length <= 3 || 'Text can only be up to 3 characters.',  
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color do you want the text to be?',
+    },
+    {
         
     }
 ]
