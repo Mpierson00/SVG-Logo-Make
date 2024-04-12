9a590e328b1a55e467e57a7240fc096d84b79d9f
Michael <mpierson00@gmail.com>
2024-04-12 12:23:32 -0400

    added functionality to index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const { Circle, Square, Triangle } = require('./lib/shapes');
+import inquirer from 'inquirer';
+import fs from 'fs/promises';
+import { Circle, Square, Triangle } from './lib/shapes.js';
+
+const shapeClasses = { Circle, Square, Triangle };
 
 const questions = [
     {
-      type: 'input',
-      name: 'text',
-      message: 'What text do you want on the logo (up to 3 characters)?',
-      validate: input => input.length <= 3 || 'Text can only be up to 3 characters.',  
+        type: 'input',
+        name: 'text',
+        message: 'What text do you want on the logo (up to 3 characters)?',
+        validate: input => input.length <= 3 || 'Text can only be up to 3 characters.',
     },
     {
         type: 'input',
@@ -18,7 +20,7 @@ const questions = [
         type: 'list',
         name: 'shape',
         message: 'Choose a shape for your logo',
-        choices: ['Circle', 'Square', 'Triangle'],
+        choices: Object.keys(shapeClasses),
     },
     {
         type: 'input',
@@ -26,3 +28,25 @@ const questions = [
         message: 'What color do you want the shape to be?',
     },
 ];
+
+const generateLogo = async () => {
+    try {
+        const answers = await inquirer.prompt(questions);
+        const { shape, shapeColor, text, textColor } = answers;
+        const shapeClass = shapeClasses[shape];
+        const shapeInstance = new shapeClass(shapeColor);
+
+        const svgContent = `
+        <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shapeInstance.render()}
+        <text x="150" y="125" font-family="Verdana" font-size="35" fill="${textColor}" text-anchor="middle">${text}</text>
+      </svg>`;
+
+      await fs.writeFile(`./examples/logo.svg`, svgContent.trim());
+      console.log('Generated logo.svg in the examples folder.');
+    } catch (error) {
+        console.error('An error occurred:', error)
+    }
+};
+
+generateLogo();
